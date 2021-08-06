a90cec57c20f173f355ccbd902725817d2763d0e
ortizjoseph7 <ortizjoseph7@gmail.com>
2021-08-06 12:00:10 -0500

    sidebar and filter

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -15,17 +15,22 @@ import {
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_SUCCESS,
+  PRODUCT_CATEGORY_LIST_SUCCESS,
+  PRODUCT_CATEGORY_LIST_REQUEST,
+  PRODUCT_CATEGORY_LIST_FAIL,
 } from '../constants/productConstants';
 
-export const listProducts = ({ seller = '', name = '' }) => async (
-  dispatch
-) => {
+export const listProducts = ({
+  seller = '',
+  name = '',
+  category = '',
+}) => async (dispatch) => {
   dispatch({
     type: PRODUCT_LIST_REQUEST,
   });
   try {
     const { data } = await Axios.get(
-      `/api/products?seller=${seller}&name=${name}`
+      `/api/products?seller=${seller}&name=${name}&category=${category}`
     );
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
@@ -33,19 +38,15 @@ export const listProducts = ({ seller = '', name = '' }) => async (
   }
 };
 
-export const listProductCategories = () => async (
-  dispatch
-) => {
+export const listProductCategories = () => async (dispatch) => {
   dispatch({
-    type: PRODUCT_LIST_REQUEST,
+    type: PRODUCT_CATEGORY_LIST_REQUEST,
   });
   try {
-    const { data } = await Axios.get(
-      `/api/products/categories`
-    );
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    const { data } = await Axios.get(`/api/products/categories`);
+    dispatch({ type: PRODUCT_CATEGORY_LIST_SUCCESS, payload: data });
   } catch (error) {
-    dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+    dispatch({ type: PRODUCT_CATEGORY_LIST_FAIL, payload: error.message });
   }
 };
 
