29ab97a84b8a38b9dfe42d0045d28ab0334b4aaf
ortizjoseph7 <ortizjoseph7@gmail.com>
2021-08-26 10:56:33 -0500

    code

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
